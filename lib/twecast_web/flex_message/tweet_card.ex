4bf8d1f4492bf77ec9a389deea0c52208fe3ca39
__saito__ <thetakahito@gmail.com>
2020-04-24 22:52:25 +0900

    Improve: Refactored to clean up code

--- lib/twecast_web/flex_message/tweet_card.ex
+++ lib/twecast_web/flex_message/tweet_card.ex
@@ -7,16 +7,52 @@ defmodule TwecastWeb.TweetCard do
   @color_gray "#8899a6"
   @color_dark "#15202b"
 
-  defp author(url, name, sub) do
-    twitter_url = "https://twitter.com/#{sub}"
-    [author_image(url), author_names(name, sub)]
-    |> box({:horizontal, spacing: "md", action: action({:uri, twitter_url})})
+  def new(tweet) do
+    %{
+      created_at: created,
+      full_text: text,
+      entities: %{
+        urls: urls
+      },
+      user: %{
+        name: name,
+        screen_name: screen_name,
+        profile_image_url_https: profile_image_url
+      }
+    } = tweet
+
+    {text, urls} = update_text(text, urls)
+
+    blocks = [
+      header: tweet_head(profile_image_url, name, screen_name),
+      body: tweet_body(tweet[:extended_entities], text, urls),
+      footer: tweet_foot(created)
+    ]
+
+    dark_styles =
+      Keyword.keys(blocks)
+      |> Enum.map(&({&1, %{backgroundColor: @color_dark}}))
+
+    new(blocks, text, {:bubble, dark_styles})
+  end
+
+  # --- header --- #
+
+  @twitter_url "https://twitter.com/"
+
+  def tweet_head(url, name, scr) do
+    profile_url = @twitter_url <> scr
+
+    [user_icon(url), user_author(name, scr)]
+    |> box({:horizontal, [
+      spacing: "md",
+      action: action({:uri, profile_url})
+    ]})
     |> box({:vertical, padding_all: "16px"})
   end
 
-  defp author_image(url) do
-    url
-    |> String.replace("_normal", "_400x400")
+  def user_icon(url) do
+    String.replace(url, "_normal", "_400x400")
     |> image(size: "full", aspect_mode: "cover")
     |> box({:vertical, [
       width: "45px",
@@ -25,10 +61,10 @@ defmodule TwecastWeb.TweetCard do
     ]})
   end
 
-  defp author_names(name, sub) do
+  def user_author(name, scr) do
     [
       {name, %{color: @color_white, gravity: "bottom"}},
-      {"@#{sub}", %{color: @color_gray, gravity: "top"}}
+      {"@#{scr}", %{color: @color_gray, gravity: "top"}}
     ]
     |> Enum.map(fn {name, opt} ->
       opt = %{flex: 1, size: "sm"} |> Map.merge(opt)
@@ -37,80 +73,26 @@ defmodule TwecastWeb.TweetCard do
     |> box({:vertical, spacing: "xs"})
   end
 
-  @crlf "\\r|\\r\\n|\\n"
-  @crlf_s "crlf"
+  # --- body --- #
 
-  defp tweet_text(text, urls) do
-    Regex.compile!(@crlf)
-    |> Regex.split(text)
-    |> Enum.map(fn line ->
-      embody_empty(line)
-      |> color_coding(urls)
-      |> text({:span, wrap: true})
-    end)
-    |> box({:vertical, []})
+  def tweet_body(media_exists, text, urls) do
+    tweet_text = tweet_text(text, urls)
+
+    case media_exists do
+      %{media: media} ->
+        Enum.map(media, &(&1.media_url_https))
+        |> social_images()
+        |> list_push([tweet_text])
+      _ -> [tweet_text]
+    end
+    |> box({:vertical, [
+      spacing: "xl",
+      padding_all: "16px",
+      padding_top: "0px"
+    ]})
   end
 
-  defp is_byte?(b), do: byte_size(b) > 0
-
-  defp embody_empty(text) do
-    unless is_byte?(text), do: @crlf_s, else: text
-  end
-
-  defp url_replace(text, urls) do
-    urls
-    |> Enum.reduce({text, []}, fn cur, {text, urls} ->
-      {
-        text |> String.replace(cur.url, cur.display_url),
-        cur.display_url |> list_push(urls)
-      }
-    end)
-  end
-
-  @at "@\\w+"
-  @at_comp "(?<=^|[^\\w#$%&*-@])(#{@at})"
-
-  @hash "#[^\\x00-\\x2f\\x3a-\\x40\\x5b-\\x5e\\x7b-\\x7e]+"
-  @hash_comp "(?<=^|[^\\w&])(#{@hash})"
-
-  @color_coding "#{@at_comp}|#{@hash_comp}"
-
-  def color_coding(text, opt \\ [])
-
-  def color_coding(text, opt) when length(opt) == 0,
-    do: color_coding!({:ok, @color_coding, text})
-
-  def color_coding(text, opt) do
-    rs = "#{@color_coding}|#{Enum.join(opt, "|")}"
-    color_coding!({:ok, rs, text})
-  end
-
-  defp color_coding!({:ok, rs, text}) do
-    r = Regex.compile!(rs)
-
-    Regex.split(r, text, include_captures: true)
-    |> Enum.filter(&is_byte?/1)
-    |> Enum.map(fn word ->
-      case word =~ r do
-        true -> @color_blue
-        false when @crlf_s == word -> @color_dark
-        false -> @color_white
-      end
-      |> (&span(word, color: &1)).()
-    end)
-  end
-
-  defp date_format_jp!(ds) do
-    use Timex
-    tz = Timezone.get("Asia/Tokyo")
-
-    Timex.parse!(ds, "%a %b %d %H:%M:%S %z %Y", :strftime)
-    |> Timezone.convert(tz)
-    |> Timex.format!("{_h24}:{m} · {YYYY}年{M}月{D}日")
-    |> String.trim()
-  end
-
-  defp social_images(urls) do
+  def social_images(urls) do
     social_ratios(length urls)
     |> Enum.reduce(urls, fn ratio, acc ->
       [head | tail] = acc
@@ -164,56 +146,87 @@ defmodule TwecastWeb.TweetCard do
     |> Enum.filter(&Enum.any?(&1.contents))
   end
 
-  def new(tweet) do
-    %{
-      created_at: created,
-      full_text: text,
-      entities: %{
-        urls: urls
-      },
-      user: %{
-        name: name,
-        screen_name: screen_name,
-        profile_image_url_https: profile_image_url
+  def update_text(text, urls) do
+    urls
+    |> Enum.reduce({text, []}, fn cur, {text, urls} ->
+      {
+        text |> String.replace(cur.url, cur.display_url),
+        cur.display_url |> list_push(urls)
       }
-    } = tweet
+    end)
+  end
 
-    {text, urls} = url_replace(text, urls)
+  @crlf "\\r|\\r\\n|\\n"
+  @crlf_s "crlf"
 
-    header =
-      author(profile_image_url, name, screen_name)
+  def tweet_text(text, urls) do
+    Regex.compile!(@crlf)
+    |> Regex.split(text)
+    |> Enum.map(fn line ->
+      embody_empty(line)
+      |> color_coding(urls)
+      |> text({:span, wrap: true})
+    end)
+    |> box({:vertical, []})
+  end
 
-    tweet_text = tweet_text(text, urls)
+  defp is_byte?(b), do: byte_size(b) > 0
 
-    body =
-      case tweet[:extended_entities] do
-        %{media: media} ->
-          media
-          |> Enum.map(&(&1.media_url_https))
-          |> social_images()
-          |> list_push(tweet_text)
-        _ -> tweet_text
+  defp embody_empty(text) do
+    unless is_byte?(text), do: @crlf_s, else: text
+  end
+
+  @at "@\\w+"
+  @at_comp "(?<=^|[^\\w#$%&*-@])(#{@at})"
+
+  @hash "#[^\\x00-\\x2f\\x3a-\\x40\\x5b-\\x5e\\x7b-\\x7e]+"
+  @hash_comp "(?<=^|[^\\w&])(#{@hash})"
+
+  @color_coding "#{@at_comp}|#{@hash_comp}"
+
+  def color_coding(text, opt \\ [])
+
+  def color_coding(text, opt) when length(opt) == 0,
+    do: color_coding!({:ok, @color_coding, text})
+
+  def color_coding(text, opt) do
+    rs = "#{@color_coding}|#{Enum.join(opt, "|")}"
+    color_coding!({:ok, rs, text})
+  end
+
+  defp color_coding!({:ok, rs, text}) do
+    r = Regex.compile!(rs)
+
+    Regex.split(r, text, include_captures: true)
+    |> Enum.filter(&is_byte?/1)
+    |> Enum.map(fn word ->
+      case word =~ r do
+        true -> @color_blue
+        false when @crlf_s == word -> @color_dark
+        false -> @color_white
       end
-      |> box({:vertical, [
-        spacing: "xl",
-        padding_all: "16px",
-        padding_top: "0px"
-      ]})
+      |> (&span(word, color: &1)).()
+    end)
+  end
 
-    footer =
-      date_format_jp!(created)
-      |> text(size: "sm", color: @color_gray)
-      |> box({:vertical, [
-        padding_all: "16px",
-        padding_top: "0px"
-      ]})
+  # --- footer --- #
 
-    blocks = [header: header, body: body, footer: footer]
+  def tweet_foot(created) do
+    date_format_jp!(created)
+    |> text(size: "sm", color: @color_gray)
+    |> box({:vertical, [
+      padding_all: "16px",
+      padding_top: "0px"
+    ]})
+  end
 
-    dark_styles =
-      Keyword.keys(blocks)
-      |> Enum.map(&({&1, %{backgroundColor: @color_dark}}))
+  defp date_format_jp!(ds) do
+    use Timex
+    tz = Timezone.get("Asia/Tokyo")
 
-    new(blocks, text, {:bubble, dark_styles})
+    Timex.parse!(ds, "%a %b %d %H:%M:%S %z %Y", :strftime)
+    |> Timezone.convert(tz)
+    |> Timex.format!("{_h24}:{m} · {YYYY}年{M}月{D}日")
+    |> String.trim()
   end
 end
